c82af65e1c1d5d5272bcbc433435390c24da82ee
m-star18 <ryuuchei@ezweb.ne.jp>
2020-05-22 10:34:41 +0900

    day04 harib01c

--- bootpack.c
+++ bootpack.c
@@ -1,11 +1,9 @@
 void HariMain(void) {
   int i;
-  for (i = 0xa0000; i <= 0xaffff; i++){
-    // > VRAMは0xa0000～0xaffffの64KBです。厳密に言うと、320x200=64000なので、62.5KBですが.
-    // > [INT(0x10); ビデオ関係](http://oswiki.osask.jp/?%28AT%29BIOS#n5884802)
-    // 　なおこのグラフィックバッファの開始番地 0xaffff は `asmhead.asm` ないで メモリアドレスを指す変数 VRAM(=0x0ff8)
-    // に保存されている. (`DWORD [VRAM]`)
-    write_mem8(i, i & 0x0f);  // 第2引数dataは 0-15 の値を期待するものである.
+  char *p;
+  for (i = 0xa0000; i <= 0xaffff; i++) {
+      p = (char *) i;
+      *p = i & 0x0f;
   }
 
   for(;;) {
--- nasmfunc.asm
+++ nasmfunc.asm
@@ -1,13 +1,5 @@
 GLOBAL  io_hlt
-GLOBAL  write_mem8
 
 io_hlt:
     HLT
-    RET
-
-write_mem8:
-    MOV     ECX, DWORD [ESP + 4]
-    MOV     AL, BYTE [ESP + 8]
-
-    MOV     BYTE [ECX], AL
     RET
