c1146d4e01f635826d6d0b9218f8fab47729d408
m-star18 <ryuuchei@ezweb.ne.jp>
2020-05-22 10:40:00 +0900

    day04 harib01d

--- bootpack.c
+++ bootpack.c
@@ -1,9 +1,11 @@
 void HariMain(void) {
   int i;
   char *p;
-  for (i = 0xa0000; i <= 0xaffff; i++) {
-      p = (char *) i;
-      *p = i & 0x0f;
+
+  p = (char *) 0xa0000;
+
+  for (i = 0; i <= 0xaffff; i++) {
+      *(p + i) = i & 0x0f;
   }
 
   for(;;) {
