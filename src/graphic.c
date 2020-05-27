76f0c7747a2ae5da5979023601dc3b5d1aa193c5
m-star18 <ryuuchei@ezweb.ne.jp>
2020-05-27 10:11:14 +0900

    day06 harib03a

--- src/graphic.c
+++ src/graphic.c
@@ -1,142 +1,4 @@
-#include "sprintf.h"
-
-#define COL8_000000 0
-#define COL8_FF0000 1
-#define COL8_00FF00 2
-#define COL8_FFFF00 3
-#define COL8_0000FF 4
-#define COL8_FF00FF 5
-#define COL8_00FFFF 6
-#define COL8_FFFFFF 7
-#define COL8_C6C6C6 8
-#define COL8_840000 9
-#define COL8_008400 10
-#define COL8_848400 11
-#define COL8_000084 12
-#define COL8_840084 13
-#define COL8_008484 14
-#define COL8_848484 15
-
-extern char hankaku[4096];
-
-void io_hlt(void);
-void io_cli(void);
-void io_out8(int port, int data);
-int io_load_eflags(void);
-void io_store_eflags(int eflags);
-
-void init_palette(void);
-void init_screen(char *vram, int xsize, int ysize);
-
-void set_palette(int start, int end, unsigned char *rgb);
-
-void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
-void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
-void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
-
-void init_mouse_cursor8(char *mouse, char bc);
-void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0, int py0, char *buf, int bxsize);
-
-void init_gdtidt(void);
-
-void load_gdtr(int limit, int addr);
-void load_idtr(int limit, int addr);
-
-struct SEGMENT_DESCRIPTOR {
-    short limit_low, base_low;
-    char base_mid, access_right;
-    char limit_high, base_high;
-};
-
-struct GATE_DESCRIPTOR {
-    short offset_low, selector;
-    char dw_count, access_right;
-    short offset_high;
-};
-
-void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar);
-void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
-
-struct BOOTINFO {
-    char cyls;
-    char leds;
-    char vmode;
-    char reserve;
-    short scrnx;
-    short scrny;
-    char *vram;
-};
-
-void HariMain(void) {
-    char *vram;
-    char str[32] = {0};
-    char mcursor[16 * 16];
-    int mx;
-    int my;
-
-    struct BOOTINFO *binfo = (struct BOOTINFO *) 0xff0;
-
-    init_palette();
-    init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
-
-    init_mouse_cursor8(mcursor, COL8_008484);
-    mx = (binfo->scrnx - 16) / 2;
-    my = (binfo->scrny - 28 - 16) / 2;
-    putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
-
-    sprintf(str, "(%d, %d)", mx, my);
-    putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, str);
-
-    sprintf(str, "scrnx = %d", binfo->scrnx);
-    putfonts8_asc(binfo->vram, binfo->scrnx, 16, 64, COL8_FFFFFF, str);
-
-    putfonts8_asc(binfo->vram, binfo->scrnx, 31, 31, COL8_000000, "Haribote OS.");
-    putfonts8_asc(binfo->vram, binfo->scrnx, 30, 30, COL8_FFFFFF, "Haribote OS.");
-
-    for (;;)
-        io_hlt();
-}
-
-void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar){
-    if (limit > 0xffffff) {
-        ar |= 0x8000; //G_bit = 1
-        limit /= 0x1000;
-    }
-
-    sd->limit_low = limit & 0xffff;
-    sd->base_low = base & 0xffff;
-    sd->base_mid = (base >> 16) & 0xff;
-    sd->access_right = ar & 0xff;
-    sd->limit_high = ((limit >> 16) & 0x0f) | ((ar >> 8) & 0xf0);
-    sd->base_high = (base >> 24) & 0xff;
-}
-
-void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar) {
-    gd->offset_low = offset & 0xffff;
-    gd->selector = selector;
-    gd->dw_count = (ar >> 8) & 0xff;
-    gd->access_right = ar & 0xff;
-    gd->offset_high = (offset >> 16) & 0xffff;
-}
-
-void init_gdtidt(void) {
-    struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *) 0x00270000;
-    struct GATE_DESCRIPTOR *idt = (struct GATE_DESCRIPTOR *) 0x0026f800;
-    int i;
-
-    for (i = 0; i < 8092; i++)
-        set_segmdesc(gdt + i, 0, 0, 0);
-
-    set_segmdesc(gdt + 1, 0xffffffff, 0x00000000, 0x4092);
-    set_segmdesc(gdt + 2, 0x0007ffff, 0x00280000, 0x409a);
-
-    load_gdtr(0xffff, 0x00270000);
-
-    for (i = 0; i < 256; i++)
-        set_gatedesc(idt + i, 0, 0, 0);
-
-    load_idtr(0x7ff, 0x0026f800);
-}
+#include "bootpack.h"
 
 void init_mouse_cursor8(char *mouse, char bc) {
     int x;
@@ -165,12 +27,15 @@ void init_mouse_cursor8(char *mouse, char bc) {
         for (x = 0; x < 16; x++) {
             if (cursor[y][x] =='*')
                 mouse[y * 16 + x] = COL8_000000;
+
             if (cursor[y][x] =='O')
                 mouse[y * 16 + x] = COL8_FFFFFF;
+
             if (cursor[y][x] =='.')
                 mouse[y * 16 + x] = bc;
         }
     }
+
 }
 
 void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0, int py0, char *buf, int bxsize) {
