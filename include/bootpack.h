a93846fac992db84cc9937e0d1999be257ab04a6
m-star18 <ryuuchei@ezweb.ne.jp>
2020-07-01 10:07:29 +0900

    day17 harib14b

--- include/bootpack.h
+++ include/bootpack.h
@@ -287,7 +287,7 @@ void inthandler20(int *esp);
 void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char act);
 void make_textbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c);
 void putfonts8_asc_sht(struct SHEET *sht, int x, int y, int c, int b, char *s, int l);
-void task_b_main(struct SHEET *sht_back);
+void console_task(struct SHEET *sheet);
 
 //mtask.c
 #define MAX_TASKS 1000 //最大タスク数
