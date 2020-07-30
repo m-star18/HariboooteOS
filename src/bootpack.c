f62cc331090734fa7b0f533cb76571395ac0197e
m-star18 <ryuuchei@ezweb.ne.jp>
2020-07-30 11:57:47 +0900

    day24 harib21a

--- src/bootpack.c
+++ src/bootpack.c
@@ -286,6 +286,9 @@ void HariMain(void) {
                     task_cons->tss.eip = (int) asm_end_app;
                     io_sti();
                 }
+                if (i == 256 + 0x57 && shtctl->top > 2) //F11
+                    sheet_updown(shtctl->sheets[1], shtctl->top - 1);
+
                 if (i == 256 + 0xfa) //キーボードがデータを正しく受け取った
                     keycmd_wait = -1;
 
